9ea0153ed611da69c899bc548f98c378bbb60d9c
unknown <unam31@gmail.com>
2024-07-30 22:28:58 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -10,27 +10,3 @@ export type {
 export type { ImagePageOutput, Text } from './types/image.page.output';
 export type { VerbosityLevel } from './types/verbosity';
 export { PDFToImageConversion } from './pdf.to.png';
-
-import express from 'express';
-import path from 'path';
-import { PDFToImageConversion } from './pdf.to.png';
-import { PDFToIMGOptions } from './types/pdf.to.image.options';
-
-const app = express();
-
-app.get('/pdf-to-png', async (req, res) => {
-  const filePath = path.join('test-data/sample.pdf');
-  const options: PDFToIMGOptions = {
-    outputFolderName: 'upload',
-    pages: [1, 2, 3],
-    disableStreams: true,
-  };
-  const pdfConversion = new PDFToImageConversion(filePath, options);
-  const pdf = await pdfConversion.convert();
-
-  res.send(pdf);
-});
-
-app.listen(3006, () => {
-  console.log('listening on *:3006');
-});
